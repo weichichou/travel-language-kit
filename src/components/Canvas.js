558a22c3fd4918766ef2ed26faa76fcbfd15b557
weichichou <weichi.cc@gmail.com>
2019-12-10 23:40:38 +0100

    Add clear and undo function

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -6,30 +6,23 @@ export default class Canvas extends React.Component {
     state = {
         showCanvas: false
     }
-    
-    handleClick = () => {
-        this.setState({showCanvas: true})
-    }
-
-    handleClose = () => {
-        this.setState({showCanvas: false})
-    }
 
     render(){
         return (<div>
-            <button onClick={this.handleClick}>Draw an item</button>
+            <button onClick={()=>{this.setState({showCanvas: true})}}>
+                Draw an item
+            </button>
+
             {this.state.showCanvas === true && 
             <div className='canvas-div'>
                 
-                <button>
-                    Clear
-                </button>
+                <button onClick={()=>{this.myCanvas.clear()}}>Clear</button>
+                <button onClick={()=>{this.myCanvas.undo()}}>Undo</button>
+                <button onClick={()=>{this.setState({showCanvas: false})}}>Close</button>
 
-                <button onClick={this.handleClose}>
-                    Close
-                </button>
-
-                <CanvasDraw className='canvas-drawing-div' brushRadius='5' lazyRadius='0' 
+                <CanvasDraw className='canvas-drawing-div' 
+                ref={element => (this.myCanvas = element)}
+                brushRadius='5' lazyRadius='0' 
                 canvasHeight='500px' canvasWidth='500px' /> 
             </div>}
         </div>
