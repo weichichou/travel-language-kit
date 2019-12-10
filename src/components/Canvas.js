82e40b392cd5f9f565b124fe310758f58000d38c
weichichou <weichi.cc@gmail.com>
2019-12-10 23:18:51 +0100

    Add close button

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,7 +3,36 @@ import CanvasDraw from "react-canvas-draw";
 import '../App.css'
 
 export default class Canvas extends React.Component {
+    state = {
+        showCanvas: false
+    }
+    
+    handleClick = () => {
+        this.setState({showCanvas: true})
+    }
+
+    handleClose = () => {
+        this.setState({showCanvas: false})
+    }
+
     render(){
-        return (<CanvasDraw brushRadius='5' lazyRadius='0'/>)
+        return (<div>
+            <button onClick={this.handleClick}>Draw an item</button>
+            {this.state.showCanvas === true && 
+            <div className='canvas-div'>
+                
+                <button>
+                    Clear
+                </button>
+
+                <button onClick={this.handleClose}>
+                    Close
+                </button>
+
+                <CanvasDraw className='canvas-drawing-div' brushRadius='5' lazyRadius='0' 
+                canvasHeight='500px' canvasWidth='500px' /> 
+            </div>}
+        </div>
+        )
     }
 }
