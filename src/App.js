90a324a3d10ad4ba279811a0913bab84e938e548
weichichou <weichi.cc@gmail.com>
2019-12-11 21:58:28 +0100

    Link to canvas component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
 import Canvas from './components/Canvas';
-import Header from './components/Header';
+// import Header from './components/Header';
 import FoodDisplay from './components/FoodDisplay';
 
 class App extends React.Component {  
   render(){
     return (<Router>
     <div className="App">
-      <Header />
-      <Route path='/' exact component={FoodDisplay} />
-      <Route path='/canvas' component={Canvas} />
+        <div className='header-div'>
+            <Link to='/canvas'>
+            <button className='btn btn-dark btn-lg'>
+                <i class="fas fa-paint-brush"></i>
+            </button>
+            </Link>
+
+            <h1>Travel Language Kit</h1>
+
+            <button className='btn btn-dark btn-lg'>
+                <i class="fas fa-globe-asia"></i>
+            </button>
+        </div>
+        <Route path='/' exact component={FoodDisplay} />
+        <Route path='/canvas' component={Canvas} />
     </div>
     </Router>)
   }
