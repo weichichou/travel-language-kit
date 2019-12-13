708793f78113a892584d7cd7a2aed4158c738942
weichichou <weichi.cc@gmail.com>
2019-12-13 12:37:23 +0100

    Add attribution to data file

--- src/components/ImgAttribute.js
+++ src/components/ImgAttribute.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { local } from '../localfood'
 
 export default class ImgAttribute extends React.Component {
+    
+    
     render(){
-        return (<div>
+        const output = local
+        .filter((item)=> item.hasOwnProperty("CC") === true)
+        .map((item)=>{
+            return <div>
+                <img src={item.imgUrl} alt={item.name} width='200px'/>
+                <p>
+                    By <a href={item.link}>{item.author}</a>
+                    , is licensed under <a href={item.CClink}>{item.CC}</a>
+                </p>
+            </div>
+        })
+        return (<div className='img-attribute'>
             <Link to='/'>
                 <button className='btn btn-dark btn-lg'>Close</button>
             </Link>
             <h3>Image Attribution</h3>
-
+            <h5>Images that are not listed below are downloaded from <a href='https://www.pexels.com/'>Pexels</a>,</h5>
+            <h5> where all photos are free to use and attribution are not required.</h5>
+            <h5 className='pexelLink' > <a href='https://www.pexels.com/photo-license/'> See photo license on Pexels</a>.</h5>
+            
+            <div>
+                {output}
+            </div>
+        
         </div>)
     }
 }
--- src/localfood.js
+++ src/localfood.js
@@ -2,47 +2,64 @@ export const local = [
     {
         "region": "nl",
         "name": "pancake",
-        "imgUrl": "images/Pannenkoek.jpg"
+        "imgUrl": "images/Pannenkoek.jpg",
+        "author": "y_zhao8",
+        "link": "https://www.flickr.com/photos/yijia/48562958982/",
+        "CC": "BY-NC-ND 2.0",
+        "CClink": "https://creativecommons.org/licenses/by-nc-nd/2.0/"
     }, {
         "region": "nl",
         "name": "stamppot",
-        "imgUrl": "images/Stamppot.jpg"
+        "imgUrl": "images/Stamppot.jpg",
+        "author": "Øystein Alsaker",
+        "link": "https://www.flickr.com/photos/oalsaker/8151484832/",
+        "CC": "BY-NC 2.0",
+        "CClink": "https://creativecommons.org/licenses/by-nc/2.0/"
     }, {
         "region": "nl",
         "name": "herring",
-        "imgUrl": "images/Haring.jpg"
+        "imgUrl": "images/Haring.jpg",
+        "author": "David Kosmos Smith",
+        "link": "https://www.flickr.com/photos/davidkosmos/5857755806/in/album-72157627015738246/",
+        "CC": "BY-NC-ND 2.0",
+        "CClink": "https://creativecommons.org/licenses/by-nc-nd/2.0/"
     }, {
         "region": "nl",
         "name": "cheese",
-        "imgUrl": "images/Cheese.jpg"
-    }, /* {
-        "region": "nl",
-        "name": "stroopwafel",
-        "imgUrl": "images/Stroopwafel.jpg"
-    }, */ 
-     {
+        "imgUrl": "images/Cheese.jpg",
+        "author": "Joseph C. Topping",
+        "link": "https://www.flickr.com/photos/jtopping/34067616382/",
+        "CC": "BY-NC-ND 2.0",
+        "CClink": "https://creativecommons.org/licenses/by-nc-nd/2.0/"
+    }, {
         "region": "zh-TW",
         "name": "beef noodles",
-        "imgUrl": "images/beef-noodles2.jpg"
-    }, /* {
-        "region": "zh-TW",
-        "name": "scallion pancakes",
-        "imgUrl": "images/scallion-pancakes.jpg"
-    }, */ 
-    {
+        "imgUrl": "images/beef-noodles2.jpg",
+        "author": "George Hong",
+        "link": "https://www.flickr.com/photos/georgehong/6028958055/",
+        "CC": "BY 2.0",
+        "CClink": "https://creativecommons.org/licenses/by/2.0/"
+    }, {
         "region": "zh-TW",
         "name": "stinky tofu",
-        "imgUrl": "images/Stinky-tofu.jpg"
-    },{
+        "imgUrl": "images/Stinky-tofu.jpg",
+        "author": "bryan...",
+        "link": "https://www.flickr.com/photos/bryansjs/18288501561/",
+        "CC": "BY-SA 2.0",
+        "CClink": "https://creativecommons.org/licenses/by-sa/2.0/"
+    }, {
         "region": "zh-TW",
         "name": "pearl milk tea",
         "imgUrl": "images/bubble-tea2.jpeg"
-    },{
+    }, {
         "region": "zh-TW",
         "name": "mango ice",
-        "imgUrl": "images/mango-ice2.jpg"
-    },
-    {
+        "imgUrl": "images/mango-ice2.jpg",
+        "author": "龜 澎湖",
+        "link": "https://www.flickr.com/photos/exkobe/8879915780/",
+        "CC": "BY 2.0",
+        "CClink": "https://creativecommons.org/licenses/by/2.0/"
+    }, {
         "region": "ja",
         "name": "matcha tea",
         "imgUrl": "images/Matcha.jpg"
@@ -50,12 +67,7 @@ export const local = [
         "region": "ja",
         "name": "sushi",
         "imgUrl": "images/sushi.jpg"
-    }, /* {
-        "region": "ja",
-        "name": "sashimi",
-        "imgUrl": "images/sashimi.jpg"
-    },  */
-    {
+    }, {
         "region": "ja",
         "name": "ramen",
         "imgUrl": "images/ramen.jpg"
